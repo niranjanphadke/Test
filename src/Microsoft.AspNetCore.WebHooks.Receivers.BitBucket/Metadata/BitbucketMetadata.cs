cc3187d60eed1803e6e672ead6f8e108de9ccc90
Doug Bunting <dougbu@microsoft.com>
2017-09-19 13:17:16 -0700

    Flesh out the Bitbucket project - mainly missing a resource filter to require UUID header

---
+++ src/Microsoft.AspNetCore.WebHooks.Receivers.BitBucket/Metadata/BitbucketMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.WebHooks.Metadata
+{
+    public class BitbucketMetadata :
+        WebHookMetadata,
+        IWebHookEventMetadata,
+        IWebHookRequestMetadataService,
+        IWebHookSecurityMetadata
+    {
+        public BitbucketMetadata()
+            : base(BitbucketConstants.ReceiverName)
+        {
+        }
+
+        public string ConstantValue => null;
+
+        public string HeaderName => BitbucketConstants.EventHeaderName;
+
+        public string PingEventName => null;
+
+        public string QueryParameterKey => null;
+
+        public WebHookBodyType BodyType => WebHookBodyType.Json;
+
+        public bool VerifyCodeParameter => true;
+    }
+}
