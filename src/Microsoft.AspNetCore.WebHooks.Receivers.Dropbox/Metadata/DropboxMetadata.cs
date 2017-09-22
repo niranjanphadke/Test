51aa42310319e8a7e3861fc6aa7a35438b21cb73
Doug Bunting <dougbu@microsoft.com>
2017-09-22 07:53:43 -0700

    Correct property name: `QueryParameterKey` -> `QueryParameterName`

--- src/Microsoft.AspNetCore.WebHooks.Receivers.Dropbox/Metadata/DropboxMetadata.cs
+++ src/Microsoft.AspNetCore.WebHooks.Receivers.Dropbox/Metadata/DropboxMetadata.cs
@@ -31,7 +31,7 @@ namespace Microsoft.AspNetCore.WebHooks.Metadata
         public string PingEventName => null;
 
         /// <inheritdoc />
-        public string QueryParameterKey => null;
+        public string QueryParameterName => null;
 
         // IWebHookRequestMetadataService...
 
